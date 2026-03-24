3ad1b0f2f00118f822aa56dfb3f179ffecc7fc0f
KingOfMusicBot <teamriya84@gmail.com>
2026-03-24 14:46:22 +0530

    Update gadd.py

--- anony/plugins/gadd.py
+++ anony/plugins/gadd.py
@@ -3,8 +3,8 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 OWNERS = "8590200017"
-from Anony import app
-from Anony.core.mongo import add_served_chat, get_assistant
+from anony import app
+from anony.core.mongo import add_served_chat, get_assistant
 
 
 @app.on_message(filters.command("gadd") & filters.user(int(OWNERS)))
